43b0a0fdcdd3bbb374cd03db50b9622446b3b003
YpL68 <yuriy.plks@gmail.com>
2022-06-09 10:36:30 +0300

    next iteration main

--- First/main.py
+++ First/main.py
@@ -1,15 +1,16 @@
-a = None
-b = None
-print(id(a))
-print(id(b))
-
-a = abs(-10)
-b = abs(-10)
-print(id(a))
-print(id(b))
-
-
+# a = None
+# b = None
+# print(id(a))
+# print(id(b))
+#
+# a = abs(-10)
+# b = abs(-10)
+# print(id(a))
+# print(id(b))
 
 
+def minus(a1, b1):
+    return a1 - b1
 
 
+print(minus(5, 6))
