81f55e03839d36d75baa310bf883fadc1bd5e9d1
YpL68 <yuriy.plks@gmail.com>
2022-06-09 18:31:00 +0300

    Научился делать импорт модулей

--- First/main.py
+++ First/main.py
@@ -1,9 +1,11 @@
-import importlib.util
-import sys
-# import module1
+# import importlib.util
+# import sys
+import First.module1 as m1
+import Two.module2 as m2
+
 # import module2
-from First.module1 import plus
-from Two.module2 import plus2
+# from First.module1 import plus
+# from Two.module2 import plus2
 
 def minus(a1, b1):
     return a1 - b1
@@ -11,5 +13,5 @@ def minus(a1, b1):
 
 if __name__ == '__main__':
     print(minus(1,2))
-    print(plus(7,9))
-    print(plus2(7,9))
+    print(m1.plus(7,9))
+    print(m2.plus2(7,9))
