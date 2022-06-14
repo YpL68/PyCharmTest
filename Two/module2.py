5bb8d220a692af8230b95b8a1973a28f213ecf3e
YpL68 <yuriy.plks@gmail.com>
2022-06-14 10:07:07 +0300

    DZ 2 final

--- Two/module2.py
+++ Two/module2.py
@@ -1,6 +1,45 @@
-def plus2(a, b):
-    return a + b
+result = None
+operand = None
+operator = None
+wait_for_number = True
+operators_list = ("+", "-", "/", "*")
 
+while True:
+    input_str = input(">> ")
+    if input_str == "=":
+        if result:
+            print(f"Result: {result}")
+        break
+    if wait_for_number:
+        if not input_str.isnumeric():
+            try:
+                operand = float(input_str)
+            except ValueError:
+                print(f"{input_str} is not a number. Try again.")
+                continue
+        else:
+            operand = int(input_str)
 
-if __name__ == '__main__':
-    print(plus2(2, 3))
+        if operator:
+            if operator == "+":
+                result += operand
+            elif operator == "-":
+                result -= operand
+            elif operator == "*":
+                result *= operand
+            else:
+                try:
+                    result /= operand
+                except ZeroDivisionError:
+                    print("ZeroDivisionError. Try again.")
+                    continue
+        else:
+            result = operand
+    else:
+        if input_str in operators_list:
+            operator = input_str
+        else:
+            print(f"{input_str} is not '+' or '-' or '/' or '*'. Try again")
+            continue
+
+    wait_for_number = not wait_for_number
