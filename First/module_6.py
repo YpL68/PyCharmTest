eb1c7467fae39e9dce69946d570bcd133a2c1a3f
YpL68 <yuriy.plks@gmail.com>
2022-07-09 19:32:00 +0300

    DZ 6 pre

--- First/module_6.py
+++ First/module_6.py
@@ -20,12 +20,12 @@ FILE_EXT_TABLE = (("images", (".JPEG", ".PNG", ".JPG", ".SVG")),
                   ("audio", (".MP3", ".OGG", ".WAV", ".AMR")),
                   ("archives", (".ZIP", ".GZ", ".TAR")))
 
-FILE_EXT_DICT = {ext: item[0] for item in FILE_EXT_TABLE for ext in item[1]}
-EXCLUDE_FOLDERS = [item[0] for item in FILE_EXT_TABLE]
-
-
 def normalize(file_name: str) -> str:
     return "_".join(re.findall(r"(\w+)", file_name.translate(TRANS)))
+
+
+def is_dir_empty(path_dir: Path) -> bool:
+    return not bool(len([x for x in path_dir.iterdir()]))
 
 
 def parsing_folder(path_dir: Path):
@@ -34,34 +34,36 @@ def parsing_folder(path_dir: Path):
             if item.name not in EXCLUDE_FOLDERS:
                 parsing_folder(item)
         else:
-            norm_file_name = normalize(item.stem) + item.suffix
-            print(norm_file_name)
-
             folder_name = FILE_EXT_DICT.get(item.suffix.upper())
             if folder_name:
-                if Path(path_dir, folder_name, item.name).exists():
-                    shutil.move(str(item), str(Path(path_dir, folder_name, item.name)))
-                else: shutil.move(str(item), str(Path(path_dir, folder_name)))
+                item.replace(Path(dir_path, folder_name, normalize(item.stem) + item.suffix))
+            else:
+                item.replace(Path(item.parent, normalize(item.stem) + item.suffix))
 
+    if is_dir_empty(path_dir): path_dir.rmdir()
+    else: path_dir.replace(Path(path_dir.parent, normalize(path_dir.name)))
 
 
 if __name__ == '__main__':
 
-    # if len(sys.argv) < 2:
-    #     sys.exit("Specify the path to the folder as an argument of the command line.")
-    # else:
-    #     dir_path = Path(sys.argv[1])
-    #     if not (dir_path.exists() and dir_path.is_dir()): sys.exit("No such folder.")
-
-    dir_path = Path(r"f:\PythonPrj\TestDir")
+    if len(sys.argv) < 2:
+        sys.exit("Specify the path to the folder as an argument of the command line.")
+    else:
+        dir_path = Path(sys.argv[1])
+        if not (dir_path.exists() and dir_path.is_dir()): sys.exit("No such folder.")
 
     # translation table creation
     for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
         TRANS[ord(c)] = l
         TRANS[ord(c.upper())] = l.upper()
 
-    for item in FILE_EXT_TABLE:
-        Path(dir_path, item[0]).mkdir(parents=False, exist_ok=True)
+    FILE_EXT_DICT = {ext: item[0] for item in FILE_EXT_TABLE for ext in item[1]}
+    EXCLUDE_FOLDERS = [item[0] for item in FILE_EXT_TABLE]
 
-    parsing_folder(dir_path)
+    for item in FILE_EXT_TABLE: Path(dir_path, item[0]).mkdir(exist_ok=True)
+
+    try:
+        parsing_folder(dir_path)
+    except:
+        sys.exit("When executing the program, an error occurred.")
 
